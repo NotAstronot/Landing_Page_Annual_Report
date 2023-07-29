528024cf0a86f48fd0fca27a213fcccc0ce5ad72
NotAstronot <feririski409@gmail.com>
2023-07-29 16:18:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
+import React from 'react';
 import './App.css';
-import SideBar from "./components/SideBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import AnnualReport from "./pages/AnnualReport";
-import Overview from "./pages/Overview";
-import EducationThePublic from "./pages/EducationThePublic";
-import EducationHealtcareProfesional from "./pages/EducationHealtcareProfesional";
-import RecognizingLeaders from "./pages/RecognizingLeaders";
-import Collaboration from "./pages/Collaboration";
-import Supporters from "./pages/Supporters";
-import BoardOfDirectors from "./pages/BoardOfDirectors";
-import FinancialHighLights from "./pages/FinancialHighLights";
-import ContactUs from "./pages/ContactUs";
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <Router>
-      <SideBar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/annualreport" element={<AnnualReport />} />
-          <Route path="/overview" element={<Overview />} />
-          <Route path="/educationthepublic" element={<EducationThePublic />} />
-          <Route path="/educationhealtcareprofesional" element={<EducationHealtcareProfesional />} />
-          <Route path="/recognizingleaders" element={<RecognizingLeaders />} />
-          <Route path="/collaboration" element={<Collaboration />} />
-          <Route path="/supporters" element={<Supporters />} />
-          <Route path="/boardofdirectors" element={<BoardOfDirectors />} />
-          <Route path="financialhighlights" element={< FinancialHighLights/>} />
-          <Route path="/contactus" element={<ContactUs />} />
-        </Routes>
-      </SideBar>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
-
 
 export default App;
